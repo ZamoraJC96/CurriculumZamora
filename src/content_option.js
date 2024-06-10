c6e916e232ee0bf2773a2d2d29396226c882c480
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2024-06-10 16:31:42 -0600

    Subiendo curriculum

--- src/content_option.js
+++ src/content_option.js
@@ -1,74 +1,93 @@
-const logotext = "JOHN";
+const logotext = "JOSÉ CARLOS";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "José Carlos Zamora Estrada",
+    description: "Ingeniero en Software y Sistemas Computacionales con experiencia en desarrollo full stack y soluciones tecnológicas, actualmente en León, Guanajuato.",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "Soy José Carlos Zamora Estrada",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "Apasionado por la tecnología",
+        second: "Desarrollador Full Stack",
+        third: "Experto en soluciones de software",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "Soy un Ingeniero en Software y Sistemas Computacionales que disfruta de la tecnología y la programación desde muy joven. Me enfoco en el desarrollo de sistemas y soluciones de software, con experiencia en front-end y back-end, aunque con una inclinación hacia el desarrollo de front-end.",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "Un poco sobre mí",
+    aboutme: "Ingeniero en Software y Sistemas Computacionales egresado de la Universidad de La Salle Bajío. Estoy en proceso de titulación y tengo una fuerte pasión por el desarrollo de software, especialmente en la creación de soluciones eficientes y escalables. Me especializo en el desarrollo full stack, con un enfoque particular en tecnologías de front-end.",
 };
-const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+
+const worktimeline = [
+    {
+        jobtitle: "Programador Backend y Frontend",
+        where: "Grupo Viseit",
+        date: "2019-2020",
+        description: "Realicé prácticas profesionales durante seis meses en Grupo Viseit, donde participé en el desarrollo de backend y frontend, levantamiento de requerimientos, y la creación de APIs utilizando Angular y Node.js con conexiones a bases de datos SQL.",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "Programador Analista de COBOL",
+        where: "Waretex",
+        date: "2022-2023",
+        description: "Trabajé en Waretex durante un año y medio como programador analista de COBOL, enfocado en la segregación y manejo de bases de datos. Aunque la naturaleza del trabajo fue rutinaria, me proporcionó una experiencia valiosa en la gestión de datos y la automatización de procesos.",
     },
     {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "Desarrollador Frontend",
+        where: "VisitApp",
+        date: "2024-actualidad",
+        description: "En VisitApp, me desempeñé como desarrollador frontend durante seis meses, trabajando con React Native para crear interfaces de usuario, implementar nuevos módulos y componentes, y realizar pruebas y testing para garantizar la calidad del software.",
     },
 ];
 
-const skills = [{
-        name: "Python",
-        value: 90,
-    },
+const skills = [
     {
-        name: "Djano",
-        value: 85,
-    },
-    {
-        name: "Javascript",
+        name: "Java",
         value: 80,
     },
     {
-        name: "React",
-        value: 60,
+        name: "C#",
+        value: 70,
     },
     {
-        name: "Jquery",
+        name: "JavaScript",
+        value: 90,
+    },
+    {
+        name: "TypeScript",
+        value: 75,
+    },
+    {
+        name: "COBOL",
+        value: 20,
+    },
+    {
+        name: "Node.js",
         value: 85,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "Angular",
+        value: 70,
     },
 ];
 
-const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+const services = [
+    {
+        title: "Desarrollo de Software a Medida",
+        description: "Ofrezco servicios de desarrollo de software a medida, proporcionando soluciones personalizadas para satisfacer las necesidades específicas de cada cliente. Me especializo en la creación de sistemas eficientes y escalables, tanto en front-end como en back-end.",
     },
     {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Análisis y Diseño de Sistemas",
+        description: "Proporciono análisis y diseño de sistemas, enfocándome en la identificación de requerimientos y la creación de arquitecturas de software robustas. Mi objetivo es asegurar que los sistemas desarrollados cumplan con los estándares de calidad y funcionalidad requeridos.",
     },
     {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Desarrollo de Interfaces de Usuario",
+        description: "Desarrollo interfaces de usuario atractivas y funcionales, utilizando tecnologías modernas como React y Angular. Me enfoco en crear experiencias de usuario intuitivas y agradables, que faciliten la interacción del usuario con el sistema.",
     },
 ];
 
@@ -136,22 +155,19 @@ const dataportfolio = [{
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+    YOUR_EMAIL: "Zamorajc96@gmail.com",
+    YOUR_FONE: "4777220029",
+    description: "Estoy disponible para consultas y proyectos relacionados con el desarrollo de software. No dudes en contactarme para discutir tus necesidades y cómo puedo ayudarte a llevar tus ideas a la realidad.",
+    YOUR_SERVICE_ID: "",
+    YOUR_TEMPLATE_ID: "",
+    YOUR_USER_ID: "",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    github: "https://github.com/ZamoraJC96",
+    linkedin: "https://www.linkedin.com/in/jos%C3%A9-carlos-zamora-estrada-6507b117a/",
 };
+
 export {
     meta,
     dataabout,
